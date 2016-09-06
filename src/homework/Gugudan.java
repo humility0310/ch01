918be6b8dff7be59f96580d05a30f10fe82b7b23
yj <bit-user@bit>
2016-09-06 15:22:37 +0900

    15:22

---
+++ src/homework/Gugudan.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=9; i++){
+			for(int j=1; j<=9; j++){
+				System.out.printf(j+"X"+i+"="+i*j+"	");
+			}
+			System.out.println("");
+		}
+	}
+}

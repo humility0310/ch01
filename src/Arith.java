364dffaf2e0600e92f46c71295082c1bf6324278
yj <bit-user@bit>
2016-09-06 10:13:26 +0900

    10:13

---
+++ src/Arith.java
@@ -0,0 +1,31 @@
+
+public class Arith {
+
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 3;
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+
+		a = a + 1;
+		System.out.println(a);
+
+		a += 1;
+		System.out.println(a);
+
+		a++;
+		System.out.println(a);
+
+		++a;
+		System.out.println(a);
+
+		System.out.println("a++ =" + a++);
+
+		System.out.println("++a =" + ++a);
+	}
+
+}
---
+++ src/Typecast1.java
@@ -0,0 +1,9 @@
+
+public class Typecast1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
